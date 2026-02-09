54574d2164905fd9bc50360be5eb467a74f09fda
jojunhyeong <jojh0323@naver.com>
2026-02-09 13:40:12 +0900

    fix: 빌드 에러 해결

--- on-fit-next/src/app/(view)/(main)/mypage/region/page.tsx
+++ on-fit-next/src/app/(view)/(main)/mypage/region/page.tsx
@@ -1,7 +1,9 @@
+export const dynamic = 'force-dynamic'
 import { updateRegionFromMyPage } from '../actions';
 import RegionSetupClient from './RegionSetupClient';
 
 export default function RegionSettingsPage() {
+
   return (
     <div className="min-h-screen bg-gradient-to-b from-background to-muted/20 flex items-center justify-center p-4">
       <RegionSetupClient saveRegion={updateRegionFromMyPage} />
